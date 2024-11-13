53359e2f481de412179dccb283a96b380b01128d
Cavalcanti56 <josecarlos.dom2022@gmail.com>
2024-11-13 17:16:04 -0300

    nova atualização

---
+++ index.php
@@ -0,0 +1,54 @@
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gerenciamento de dados</title>
+</head>
+<body>
+    <header>
+        <h1>Gerenciamento de dados</h1>
+        <nav>
+            <a href="index.php?menuop=home">home</a>
+            <a href="index.php?menuop=contatos">contatos</a>
+            <a href="index.php?menuop=tarefas">tarefas</a>
+            <a href="index.php?menuop=eventos">eventos</a>
+        </nav>
+    </header>
+    <main>
+    <?php
+        $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+        switch ($menuop) {
+            case 'home':
+                include("paginas/home/home.php");
+                break;
+            
+            case 'contatos':
+                include("paginas/contatos/contatos.php");
+                break;
+                
+                case 'inserir-contato':
+                    include("paginas/contatos/inserir-contato.php");
+                    break;
+
+            case 'cad-contato':
+                include("paginas/contatos/cad-contato.php");
+                break;
+
+            case 'tarefas':
+                include("paginas/tarefas/tarefas.php");
+            break;
+
+            case 'eventos':
+                include("paginas/eventos/eventos.php");
+            break;
+
+            default:
+                include("paginas/home/home.php");
+                break;
+        }
+
+    ?>
+    </main>
+    
+</body>
+</html>
---
+++ paginas/home/home.php
@@ -0,0 +1,1 @@
+<h3>Pagína principal</h3>
