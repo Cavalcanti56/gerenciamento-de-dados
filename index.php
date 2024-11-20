bbce017ff482bb2f0006670a2ab75324a124e206
Rafael Luna <rafael.souza05@cs.unipe.edu.br>
2024-11-20 14:48:49 -0300

    correções do códigov2

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    include("./db/conexao.php");
+    session_start();
+
+        $rs = mysqli_query($conexao, $sql);
+        $dados = mysqli_fetch_assoc($rs);
+        $linha = mysqli_num_rows($rs);
+?>
+<!DOCTYPE html>
 <html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
@@ -17,8 +26,8 @@
             </a>
             <div class="collapse navbar-collapse" id="conteudonavbarsuportado">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a class="nav-link" href="index.php?menuop=home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?menuop=contatos">Contatos</a></li>
+                    <li class="nav-item active"><a class="nav-link" href="index.php?menuop=home">home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?menuop=contatos">contatos</a></li>
                 </ul>
             </div>
         </nav>
@@ -27,34 +36,28 @@
     <main>
 <div class="container">
     <?php
-        $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
-        switch ($menuop) {
+        $menuop = (isset($_GET['menuop']))?$_GET['menuop']:'home';
+        switch($menuop){
             case 'home':
-                include("paginas/home/home.php");
+                include("./paginas/home/home.php");
                 break;
-            
             case 'contatos':
-                include("paginas/contatos/contatos.php");
+                include("./paginas/contatos/contato.php");
                 break;
-
-                case 'cad-contato':
-                    include("paginas/contatos/cad-contato.php");
-                    break;
-                
-                case 'inserir-contato':
-                    include("paginas/contatos/inserir-contato.php");
-                    break;
-
-                case 'editar-contato':
-                    include("paginas/contatos/editar-contato.php");
-                    break;
-
-                case 'excluir-contato':
-                        include("paginas/contatos/excluir-contato.php");
-                        break;
-
-                case 'atualizar-contato':
-                    include("paginas/contatos/atualizar-contato.php");
+            case 'cad-contato':
+                include("./paginas/contatos/cad-contatos.php");
+                break;   
+            case 'inserir-contato':
+                    include("./paginas/contatos/inserir-contato.php");
+                    break;  
+            case 'editar-contato':
+                    include("./paginas/contatos/editar-contato.php");
+                    break; 
+            case 'atualizar-contato':
+                    include("./paginas/contatos/atualizar-contato.php");
+                    break;                                       
+            case 'excluir-contato':
+                    include("./paginas/contatos/excluir-contato.php");
                     break;
             default:
                 include("paginas/home/home.php");
@@ -64,10 +67,11 @@
     ?>
     </div>
     </main>
-    <footer class="container-fluid fixed-nottom bg-dark">
-        <div class="text.center">Sistema de Agenda</div>
+    <footer class="container-fluid fixed-bottom bg-dark">
+        <div class="text-center text-white py-2">Sistema de Agenda</div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous" defer></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous" defer></script>
 </body>
 </html>
