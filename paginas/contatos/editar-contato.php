c438921dc67f10556de389d8862e93d787e2bb10
brunotan14 <brunotan63@gmail.com>
2024-11-20 19:18:45 -0300

    correção de código da página editar-contato.php

--- paginas/contatos/editar-contato.php
+++ paginas/contatos/editar-contato.php
@@ -1,46 +1,46 @@
 <?php
-$idcontato = $_GET["idContato"];
+$idContato = $_GET["idContato"];
 
-$sql = "SELECT * FROM tbcontatos WHERE idContato= {$idContato}";
-$rs = mysqli_query($conexao,$sql) or die("Erro ao recuperar os dados do registro." .mysqli_error($conexao));
+$sql = "SELECT * FROM tbcontatos WHERE idContato = {$idContato}";
+$rs = mysqli_query($conexao, $sql) or die("Erro ao recuperar os dados do registro: " . mysqli_error($conexao));
 $dados = mysqli_fetch_assoc($rs);
 ?>
 <header>
-    <h3>Editar contato</h3>
+    <h3>Editar Contato</h3>
 </header>
 
-<div>
+<div class="formulario-editar">
     <form action="index.php?menuop=atualizar-contato" method="post">
-<div>
-    <label for="idContato">Nome</label>
-    <input type="text" name= "idContato"  value="<?=$dados["idContato"]?>">
-</div>
-<div>
-    <label for="nomeContato">Nome</label>
-    <input type="text" name= "nomeContato" value="<?=$dados["nomeContato"]?>>
-</div>
-<div>
-    <label for="emailContato">E-mail</label>
-    <input type="email" name= "emailContato" value="<?=$dados["emailContato"]?>>
-</div>
-<div>
-    <label for="telefoneContato">Telefone</label>
-    <input type="text" name= "telefoneContato" value="<?=$dados["telefoneContato"]?>>
-</div>
-<div>
-    <label for="enderecoContato">Endereço</label>
-    <input type="text" name= "enderecoContato" value="<?=$dados["enderecoContato"]?>>
-</div>
-<div>
-    <label for="sexoContato">Sexo Contato</label>
-    <input type="text" name= "sexoContato" value="<?=$dados["sexoContato"]?>>
-</div>
-<div>
-    <label for="dataNascContato">Data de Nascimento</label>
-    <input type="date" name= "dataNascContato" value="<?=$dados["dataNascContato"]?>>
-    <div>
-        <input type="submit" value="Atualizar"name = "btnAtualizar">
-    </div>
-</div>
+        <div>
+            <label for="idContato">ID</label>
+            <input type="text" name="idContato" id="idContato" value="<?= $dados["idContato"] ?>" readonly>
+        </div>
+        <div>
+            <label for="nomeContato">Nome</label>
+            <input type="text" name="nomeContato" id="nomeContato" value="<?= $dados["nomeContato"] ?>" required>
+        </div>
+        <div>
+            <label for="emailContato">E-mail</label>
+            <input type="email" name="emailContato" id="emailContato" value="<?= $dados["emailContato"] ?>" required>
+        </div>
+        <div>
+            <label for="telefoneContato">Telefone</label>
+            <input type="text" name="telefoneContato" id="telefoneContato" value="<?= $dados["telefoneContato"] ?>" required>
+        </div>
+        <div>
+            <label for="enderecoContato">Endereço</label>
+            <input type="text" name="enderecoContato" id="enderecoContato" value="<?= $dados["enderecoContato"] ?>" required>
+        </div>
+        <div>
+            <label for="sexoContato">Sexo</label>
+            <input type="text" name="sexoContato" id="sexoContato" value="<?= $dados["sexoContato"] ?>" required>
+        </div>
+        <div>
+            <label for="dataNascContato">Data de Nascimento</label>
+            <input type="date" name="dataNascContato" id="dataNascContato" value="<?= $dados["dataNascContato"] ?>" required>
+        </div>
+        <div>
+            <input type="submit" value="Atualizar" name="btnAtualizar">
+        </div>
     </form>
 </div>
