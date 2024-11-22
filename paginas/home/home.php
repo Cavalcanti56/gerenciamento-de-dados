46b41c12646f2b1898a53ccc25bea9b55bcccc62
brunotan14 <brunotan63@gmail.com>
2024-11-22 15:36:10 -0300

    estilização

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -1,5 +1,63 @@
+<style>
+
+</style>
+
 <h3>Pagina Inicial</h3>
 <hr>
 <h2>Então, o oque seria um contato?</h2>
 <p>Uma pessoa ou organização que mostra interesse em seu serviço ou negócio é um 'lead'. Quando um cliente potencial compra seu produto ou serviço, ele se torna seu cliente e é salvo em seu banco de dados como um 'contato'. Um contato pode ser um indivíduo ou uma organização.</p>
 <p>A tecnologia de software de hoje oferece sistemas de gerenciamento de dados abrangentes para gerenciar os dados do cliente. Eles ajudam a melhorar a categorização e tornam a pesquisa mais fácil.</p>
+
+<div class="mb-6">
+<h2>Necessidade do processo de gerenciamento de contatos</h2>
+<p>A vantagem que o gerenciamento de contatos confere às empresas é a capacidade de garantir um maior envolvimento com seus clientes. Além disso, o repositório unificado de informações do cliente permite que todos em sua organização acessem os dados de forma integrada.</p>
+<p>Aqui estão as principais vantagens do gerenciamento de contatos:</p>
+</div>
+
+<div class="row">
+
+<div class="col align-self-center">
+  
+    <div class="d-flex">
+      <!-- Badge -->
+      <div class="text-success">
+        <svg class="icon icon-check1 h2 font-weight-bold"><use xlink:href="#icon-check1"></use></svg>
+      </div>
+
+      <div class="ml-2 mb-0">
+        <p class="gt-block"><strong>Repositório de dados unificado</strong>: Um repositório de dados consolidado que simplifica o acesso aos dados para todos os funcionários da organização.</p>
+      </div>
+    </div>
+  
+    <div class="d-flex">
+      <!-- Badge -->
+      <div class="text-success">
+        <svg class="icon icon-check1 h2 font-weight-bold"><use xlink:href="#icon-check1"></use></svg>
+      </div>
+
+      <div class="ml-2 mb-0">
+        <p class="gt-block"><strong>Coleta de dados eficiente</strong>: Capacidade de armazenar informações do cliente com baixa sobrecarga e alta eficiência.</p>
+      </div>
+    </div>
+
+    <div class="d-flex">
+      <!-- Badge -->
+      <div class="text-success">
+        <svg class="icon icon-check1 h2 font-weight-bold"><use xlink:href="#icon-check1"></use></svg>
+      </div>
+
+      <div class="ml-2 mb-0">
+        <p class="gt-block"><strong>Percepções poderosas do cliente</strong>: Rastreie os pontos problemáticos, os interesses e as necessidades do cliente com eficiência, utilizando-o para impulsionar as vendas.</p>
+      </div>
+    </div>
+  
+    <div class="d-flex">
+      <!-- Badge -->
+      <div class="text-success">
+        <svg class="icon icon-check1 h2 font-weight-bold"><use xlink:href="#icon-check1"></use></svg>
+      </div>
+
+      <div class="ml-2 mb-0">
+        <p class="gt-block"><strong>Aproveite o marketing de gotejamento</strong>: Envie mensagens pré-escritas aos clientes em intervalos planejados por meio de sites sociais e e-mails para impulsionar as vendas.</p>
+      </div>
+    </div>
