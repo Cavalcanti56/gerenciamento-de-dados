e58e19a9595545c4dad663e0028de086b8f0e285
bernardobbl <bernardobarcellosleite@gmail.com>
2024-11-22 09:14:56 -0300

    excluir certo

--- paginas/contatos/excluir-contato.php
+++ paginas/contatos/excluir-contato.php
@@ -2,9 +2,9 @@
     <h3>Excluir Contato</h3>
 </header>
 <?php
-$idContato = mysqli_real_escape_string($conexao,$_get["idContato"]);
-$sql = "DELETE FROM tbcontatos WHERE idContatos= '{$idContato}";
+$idContato = mysqli_real_escape_string($conexao,$_GET["idContato"]);
+$sql = "DELETE FROM tbcontatos WHERE idContato = '{$idContato}'";
 
-mysqli_query($conexao,$sql) or die("Erro ao excluir o registro. ". mysqli_error($conexão));
+mysqli_query($conexao,$sql) or die("Erro ao excluir o registro. ". mysqli_error($conexao));
 echo "Registro excluido com sucesso!";
 ?>
