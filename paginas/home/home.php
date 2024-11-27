48ee6c7b010ce7c2ac2aaa4a94e67c1e457dd799
Rafael Luna <rafael.souza05@cs.unipe.edu.br>
2024-11-27 19:58:25 -0300

    finalizando a home

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -1,16 +1,59 @@
 <style>
+    h3 {
+            font-size: 3rem;
+            font-weight: 700;
+            color: black;
+            text-align: center;
+            position: relative;
+            padding: 0.5rem;
+            transition: all 0.3s ease;
+        }
 
+        h3::before {
+            content: '';
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+            width: 50%;
+            height: 5px;
+            background-color: #007bff;
+            border-radius: 2px;
+            opacity: 0;
+            transition: all 0.3s ease;
+        }
+
+        h3:hover {
+            color: white;
+            transform: scale(1.1);
+        }
+
+        h3:hover::before {
+            opacity: 1;
+            width: 100%;
+}
 </style>
 <br>
-<h3>Pagina Inicial</h3>
 <hr>
-<h2>Então, o oque seria um contato?</h2>
+<h3>Página Inicial</h3>
+<hr>
+
+<h2><strong>Então, o oque seria um contato?</strong></h2>
 <p>Uma pessoa ou organização que mostra interesse em seu serviço ou negócio é um 'lead'. Quando um cliente potencial compra seu produto ou serviço, ele se torna seu cliente e é salvo em seu banco de dados como um 'contato'. Um contato pode ser um indivíduo ou uma organização.</p>
 <p>A tecnologia de software de hoje oferece sistemas de gerenciamento de dados abrangentes para gerenciar os dados do cliente. Eles ajudam a melhorar a categorização e tornam a pesquisa mais fácil.</p>
+<br>
 
 <div class="mb-6">
-<h2>Necessidade do processo de gerenciamento de contatos</h2>
+<h2><strong>Necessidade do processo de gerenciamento de contatos:</strong></h2>
 <p>A vantagem que o gerenciamento de contatos confere às empresas é a capacidade de garantir um maior envolvimento com seus clientes. Além disso, o repositório unificado de informações do cliente permite que todos em sua organização acessem os dados de forma integrada.</p>
 <p>Aqui estão as principais vantagens do gerenciamento de contatos:</p>
+
+<p><b>• Organização e Acesso Rápido:</b><br>
+Armazenamento centralizado de todos os contatos, permitindo fácil acesso a informações importantes (nomes, números de telefone, e-mails, endereços, etc.).
+Facilita a busca por contatos específicos, economizando tempo e esforço.</p>
+
+<p><b>• Facilidade de Atualizações:</b><br>
+À medida que seus contatos mudam de número ou e-mail, você pode atualizar essas informações de maneira simples e rápida, evitando aquele famoso "deve estar desatualizado".</p>
+<p><b>• Evita Perder Contatos:</b><br>
+    Nada pior que perder aquele número importante ou ficar sem o e-mail de alguém que você precisa falar. Com o gerenciador de contatos, você reduz a chance de perder essas informações valiosas.</p>
 </div>
 
